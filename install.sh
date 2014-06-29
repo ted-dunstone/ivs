af4d3be034af30e864b6990ace3930622f43d72b
Ted Dunstone <ted@biometix.com>
2014-06-29 16:19:20 +1000

    install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo easy_install werkzeug
+sudo easy_install -U scipy 
+sudo easy_install -U scikit-learn 
+sudo easy_install redis
+sudo easy_install jinja2
+
+sudo add-apt-repository ppa:rwky/redis
+sudo apt-get update
+sudo apt-get install redis-server
+sudo apt-get install python-numpy python-scipy
