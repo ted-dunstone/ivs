989267cbc8d652455606ff00808572d7a5fb7ac3
Biometix <ted@biometix.com>
2014-07-04 13:13:55 +1000

    ignore update

--- start_demo.sh
+++ start_demo.sh
@@ -9,7 +9,7 @@ python ivs.py @Configs/country3.ini &
 python ivs.py @Configs/country4.ini &
 #nohup python ivs.py @child.ini &
 sleep 10
-google-chrome http://127.0.0.1:8001 http://127.0.0.1:8001/log http://localhost:8002/demo/verify http://localhost:15672/#/exchanges/%2F/OBTB http://127.0.0.1:8002/demo/upload &
+google-chrome http://127.0.0.1:8001 http://127.0.0.1:8001/log http://localhost:8002/demo/upload http://localhost:8003/demo/upload http://localhost:8002/demo/verify http://localhost:15672/#/exchanges/%2F/OBTB http://127.0.0.1:8002/demo/upload &
 while true; do
     sleep 100
 done
